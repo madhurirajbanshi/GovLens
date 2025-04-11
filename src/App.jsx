a0e0636fc6af987b63966dcaff822c3395c5dca7
madhurirajbanshi <madhurirajbanshi318@gmail.com>
2025-04-11 21:32:47 +0545

    Refactor code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-// App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/Homepage";
 import PoliticianArticle from "./Pages/PoliticianArticle";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <Router>
@@ -9,6 +9,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/politician/:name" element={<PoliticianArticle />} />
       </Routes>
+    <Footer/>
     </Router>
   );
 }
